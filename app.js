3ceb3d672cbed406e8f3481748b81e0df6f8944b
Muhammad Asif1163 <bibbasheikh11334@gmail.com>
2023-10-23 23:45:42 +0500

    2 files create

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+
+
+var userName = "Muhammad Asif"
+var myAge = "25 year old"
+var course = "Certified Mobile Application Development"
+var PIZZA = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+var email = "My email address is example@example.com"
+var education = "I am trying to learn from the Book A smarter way to learn JavaScript"
+
+alert(userName)
+alert(myAge)
+alert(course)
+alert(PIZZA)
+alert(email)
+alert(education)
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
